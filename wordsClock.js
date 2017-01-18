3b92a12add44c99ac6465a09099ea62b9ec52fd6
AllenGuo20150530 <guoyu2015guoallen@hotmail.com>
2017-01-18 14:52:48 +0800

    完成基本功能编写

--- wordsClock.js
+++ wordsClock.js
@@ -5,18 +5,16 @@ const log =function() {
 }
 
 // 获取时间参数
-var getOptions = function() {
+const getOptions = function() {
     var options = {}
-    // var miniTime = document.querySelector('#id-select-minitime').value
     options.miniTime = Number(document.querySelector('#id-select-minitime').value)
     options.miniRecite = Number(document.querySelector('#id-select-minirecite').value)
     options.times = Number(document.querySelector('#id-select-times').value)
-    // options.recite = Number(document.querySelector('#id-select-recite').value)
     return options
 }
 
-// 转换为毫秒数
-var transfer2micro = function(minutes) {
+// 将时间参数转换为毫秒数
+const transfer2micro = function(minutes) {
     var microSec = minutes * 60 * 1000
     return microSec
 }
@@ -24,44 +22,46 @@ var transfer2micro = function(minutes) {
 const itsTime = function() {
     var audio = document.querySelector('audio')
     audio.setAttribute('src', 'alarm-clock.wav')
+    log(9)
+    setTimeout(function(){
+        audio.removeAttribute('src')
+    }, 3000)
 }
-var options = getOptions()
-var delayTime = transfer2micro(options.miniTime) + transfer2micro(options.miniRecite)
-var delayTimeClear = delayTime * (options.times)
-var flag = 0
-var inter = setInterval(function() {
-    itsTime(0)
-    log(flag)
-    flag += 1
-}, delayTime)
-setTimeout(function(){
-    clearInterval(inter)
-}, delayTimeClear)
 
-
-var getMemoryTime = function(options) {
+// 获得数组arr，内有需要提示开始背诵的时间
+const getMemoryTime = function(options) {
     var arr = new Array(options.times)
     var len = arr.length
     arr[0] = options.miniTime
-    for (var i = 0; i + 1 < len; i++) {
+    for (let i = 0; i + 1 < len; i++) {
         arr[i + 1] = arr[i] + 6
     }
     return  arr
 }
-var getReciteTime = function(options) {
+// 获得数组，内有提示复习的时间
+const getReciteTime = function(options) {
     var arr = new Array(options.times)
     var len = arr.length
     arr[0] = options.miniTime + 1
-    for (var i = 0; i + 1 < len; i++) {
+    for (let i = 0; i + 1 < len; i++) {
         arr[i + 1] = arr[i] + 6
     }
     return  arr
 }
-var setTime = function(array) {
+// 依据所传入的数组参数，分别设置提醒时间
+const setTime = function(array) {
     for (let i = 0; i < array.length; i++) {
-        var delay = 1000 * testArray[i]
+        var delay = transfer2micro(array[i])
         setTimeout(function() {
             itsTime()
         }, delay)
     }
+}
+// 主函数入口
+const __main = function() {
+    var options = getOptions()
+    var reciteTime = getReciteTime(options)
+    var memoryTime = getMemoryTime(options)
+    setTime(memoryTime)
+    setTime(reciteTime)
 }
